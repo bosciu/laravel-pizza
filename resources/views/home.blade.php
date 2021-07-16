244dab4358480547396bb36d61b74395fafbc87f
Antonioflo13 <floreantonio@hotmail.it>
2021-07-16 15:52:11 +0200

    route, view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,13 @@
 
 @section('content')
     <h1>Homepage</h1>
+    <section>
+        <article>
+            @foreach ($pizza as $item)
+                <h2>{{ $item->name }}</h2>
+                <h2>{{ $item->price }}</h2>
+                <h2>{{ $item->ingredients }}</h2>
+            @endforeach
+        </article>
+    </section>
 @endsection
