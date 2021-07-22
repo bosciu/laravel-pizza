50adb009c6a04b7a3a47c76d1fdf82d2bfab36b3
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-07-22 16:07:58 +0200

    task: store-pizza-controller

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,4 @@
-<header class="container my-4">
+<header class="container d-flex my-4">
     <a href="{{ route('pizzas.index') }}">elenco pizze</a>
+    <a href="{{ route('pizzas.create') }}">crea pizza</a>
 </header>
